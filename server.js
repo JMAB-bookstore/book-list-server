84a25036aad353f6197db7f1c10e1cad967c0689
Justin Morris <justinjodymorris@gmail.com>
2018-04-24 14:09:51 -0700

    added file to add to books to database

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const pg = require('pg');
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
@@ -25,3 +26,47 @@ app.get('/awesomebooks', (request, response) => {
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+
+loadDB();
+
+function loadBooks() {
+  client.query('SELECT COUNT(*) FROM books')
+    .then(result => {
+      if (!parseInt(result.rows[0].count)) {
+        fs.readFile('./books.json', 'utf8', (err, fd) => {
+          JSON.parse(fd).forEach(ele => {
+            client.query(`
+            INSERT INTO
+            books(title, author, isbn, image_url, description) VALUES
+           ($1, $2, $3, $4, $5)
+            `,
+            [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
+            );
+          });
+        });
+      }
+    });
+}
+
+
+function loadDB() {
+
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    books (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      author TEXT,
+      isbn VARCHAR(50),
+      image_url VARCHAR(200),
+      description VARCHAR
+    );`
+  )
+    .then(data => {
+      loadBooks(data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
