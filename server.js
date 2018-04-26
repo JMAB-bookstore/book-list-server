ca10e615fe49700f4c22ed6df3d06380ec87d070
Andrew Baik <andrewbaik@outlook.com>
2018-04-26 11:55:13 -0700

    Creating new book functionality in place

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ client.connect();
 client.on('error', err => console.error(err));
 
 app.use(express.json());
-app.use(express.urlendcoded({extended:true}));
+app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
 app.get('/', (request, response) => response.send('AWESOME BOOKS!'));
@@ -21,15 +21,28 @@ app.get('/', (request, response) => response.send('AWESOME BOOKS!'));
 app.get('/api/v1/books', (request, response) => {
   client.query('SELECT * FROM books;')
     .then(results => response.send(results.rows))
-    .catch(console.error);
+    .catch( err => app.errorView.errorCallBack(err));
 });
 
 app.get('/api/v1/books/:id', (request, response) => {
   let key = `SELECT * FROM books WHERE id=$1`;
   let value = [request.params.id];
   client.query(key, value)
-    .then( results => response.send(results.rows[0]) ) // included 0 index
-    .catch( console.error);
+    .then( results => response.send(results.rows[0]) )
+    .catch( err => app.errorView.errorCallBack(err));
+});
+
+app.post('/api/v1/books', (request, response) => {
+  let key = `INSERT INTO books(title, author, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5);`;
+  let value = [
+    request.body.title,
+    request.body.author,
+    request.body.isbn,
+    request.body.image_url,
+    request.body.description];
+  client.query(key, value)
+    .then( () => response.send( 'New Book added to db'))
+    .catch( err => app.errorView.errorCallBack(err));
 });
 
 
